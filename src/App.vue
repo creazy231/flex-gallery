1983c178072eea9850c33b96ecd2dc78d215417d
Tobias Thiele <thiele231@web.de>
2020-04-21 10:47:25 +0200

    Revert "Update README.md"
    This reverts commit 72f67fb051d7a6d94ab301cdf270bdc162f90e84.

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,6 @@
 <template>
 	<div id="app">
-		<flex-gallery
-				images="https://picsum.photos/id/10/1024/768,https://picsum.photos/id/1000/1024/768,https://picsum.photos/id/1003/1024/768,https://picsum.photos/id/1004/1024/768,https://picsum.photos/id/1005/1024/768,https://picsum.photos/id/1012/1024/768,https://picsum.photos/id/1015/1024/768,https://picsum.photos/id/1020/1024/768"
-				thumbnails="https://picsum.photos/id/10/300/300,https://picsum.photos/id/1000/300/300,https://picsum.photos/id/1003/300/300,https://picsum.photos/id/1004/300/300,https://picsum.photos/id/1005/300/300,https://picsum.photos/id/1012/300/300,https://picsum.photos/id/1015/300/300,https://picsum.photos/id/1020/300/300"
-				border="2px"
-				border-color="orange"
-				col="4"
-				gap="1rem"
-		/>
+		<flex-gallery images="https://picsum.photos/id/10/200/300,https://picsum.photos/id/1000/200/300,https://picsum.photos/id/1003/200/300,https://picsum.photos/id/1004/200/300,https://picsum.photos/id/1005/200/300,https://picsum.photos/id/1012/200/300,https://picsum.photos/id/1015/200/300,https://picsum.photos/id/1020/200/300" />
 	</div>
 </template>
 
